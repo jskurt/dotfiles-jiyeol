7450fe301dc2049f73857546cdf2cd660dca72e6
Jiyeol Lee <ka.felix496@gmail.com>
2024-06-07 21:19:29 -0400

    add codesnap plugin

--- .nvim/lua/custom/keymaps.lua
+++ .nvim/lua/custom/keymaps.lua
@@ -58,6 +58,9 @@ vim.cmd [[
   nnoremap <Leader>cl :silent put=['console.log(\"<C-r><C-w>\", <C-r><C-w>);']<CR>-2==+
   xnoremap <Leader>cl y:<C-u>silent put=['console.log(\"<C-r>"\", <C-r>");']<CR>-2==+
 
+  xnoremap <Leader>cc :CodeSnap<CR>
+  xnoremap <Leader>cs :CodeSnapSave<CR>
+
   " replace
   nnoremap <Leader>rp :%s///g<Left><Left>
   nnoremap <Leader>rc :%s///gc<Left><Left><Left>
